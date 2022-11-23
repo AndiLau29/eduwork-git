1fc9183588bc1360e744adccb026fd7927c7b1ac
Fadli <fadliadadakholqi15@gmail.com>
2022-11-23 21:30:47 +0700

    tugas custom command

---
+++ customcomand.js
@@ -0,0 +1,44 @@
+/// <reference types="cypress" />
+
+describe('Working with inputs', () => {
+    it('Visit the website', () => {
+        cy.visit('http://zero.webappsecurity.com/login.html')
+        cy.url('').should('include', 'login.html')
+    });
+    it('Should fill username', () => {
+        cy.get('#user_login').clear()
+        cy.get('#user_login').type('username')
+    });
+
+    it('Should fill password', () => {
+        cy.get('#user_password').clear()
+        cy.get('#user_password').type('password')
+    });
+
+    it('Should fill checkbox', () => {
+        cy.get('#user_remember_me').click()
+        cy.get('#user_remember_me').type('checkbox')
+    });
+
+    it('Should try to login', () => {
+        cy.fixture("user").then(user => {
+            const username = user.username
+            const password = user.password
+            cy.login(username, password)
+            
+            // cy.get('.onlineBankingMenu').contains('Online Banking').click()
+            cy.visit('http://zero.webappsecurity.com/bank/pay-bills.html')
+            
+            cy.get('input[name="amount"]').clear()
+            cy.get('input[name="amount"]').type(10000)
+
+            cy.get('input[name="date"]').clear()
+            cy.get('input[name="date"]').type('2022-11-01')
+
+            cy.get('input[name="description"]').clear({ force: true })
+            cy.get('input[name="description"]').type('Bayar')
+
+            cy.get('#pay_saved_payees').click()
+        });
+    });
+});
--- fixturesandstatistic.js
+++ fixturesandstatistic.js
@@ -1,18 +1,28 @@
-///<reference types="cypress" /> 
+/// <reference types="cypress" />
 
 describe('Fixtures and statistic data', () => {
-    before(() => {
+    it('visit the website', () => {
         cy.visit('https://www.saucedemo.com/')
         cy.url('').should('include', '')
     });
 
-    it('Should try entire web features', () => {
-        cy.fixture("user").then(user => {
-            const username = user.username
-            const password = user.password
+    it('Fill the invalid username', () => {
+        cy.get('#user-name').clear()
+        cy.get('#user-name').type('username')
+    });
+
+    it('Fill the invalid password', () => {
+        cy.get('#password').clear()
+        cy.get('#password').type('password')
+    });
+
+    it('Should try to login', () => {
+        cy.fixture("example").then(user => {
+            const username = user.usernamesauce
+            const password = user.passwordsauce
             
             cy.get('#user-name').clear()
-            cy.get('#user-name').type('standard_user')
+            cy.get('#user-name').type('standar_user')
 
             cy.get('input[name="password"]').clear()
             cy.get('input[name="password"]').type('secret_sauce')
@@ -30,6 +40,9 @@ describe('Fixtures and statistic data', () => {
             cy.get('#react-burger-menu-btn').click()
             cy.get('#react-burger-menu-btn').should('have.text', 'Open Menu')
 
+            cy.get('.select_container').click()
+            cy.get('.active_option').should('contain.text', 'Name (A to Z)')
+        
         })
     });
 });
